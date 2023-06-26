14c472fabb69fab3a1d9343e9458d1b2110b0b03
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-06-26 23:09:02 +0530

    update

--- server.js
+++ server.js
@@ -2,23 +2,7 @@ import express from 'express';
 import { posts } from './posts.js';
 const app = express();
 
-const isLoggedin = (req, res, next) => {
-  let token;
-  if (
-    req.cookies?.token ||
-    (req.headers?.authorization &&
-      req.headers?.authorization.startsWith('Bearer'))
-  ) {
-    token = req.cookies?.token || req.headers.authorization.split(' ')[1];
-  }
-  if (!token) {
-    return res.status(401).send('Unauthorized to access this route');
-  }
-
-  return next();
-};
-
-app.get('/post', isLoggedin, (_req, res) => {
+app.get('/post', (_req, res) => {
   res.status(200).json({ success: true, posts: posts });
 });
 
