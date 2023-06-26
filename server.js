1bda286cd228ed262f8c4e0a26d60db037195149
Vimal Kumar <Vimal2540.vk@gmail.com>
2023-06-26 23:07:15 +0530

    first commit

--- server.js
+++ server.js
@@ -2,10 +2,26 @@ import express from 'express';
 import { posts } from './posts.js';
 const app = express();
 
-app.get('/post', (_req, res) => {
+const isLoggedin = (req, res, next) => {
+  let token;
+  if (
+    req.cookies?.token ||
+    (req.headers?.authorization &&
+      req.headers?.authorization.startsWith('Bearer'))
+  ) {
+    token = req.cookies?.token || req.headers.authorization.split(' ')[1];
+  }
+  if (!token) {
+    return res.status(401).send('Unauthorized to access this route');
+  }
+
+  return next();
+};
+
+app.get('/post', isLoggedin, (_req, res) => {
   res.status(200).json({ success: true, posts: posts });
 });
 
-app.listen(4000, () => {
-  console.log('Server running at port 4000');
+app.listen(8000, () => {
+  console.log('Server running at port 8000');
 });
